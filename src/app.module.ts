af6510ec4be31a98615544cc0fa4f40f46cd8d29
Thisen Nambukara <thisennambukara@gmail.com>
2026-07-10 12:05:11 +0530

    Create User, Service, and Booking models
    Create User, Service, and Booking models

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ServicesModule } from './services/services.module';
+import { BookingsModule } from './bookings/bookings.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, ServicesModule, BookingsModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
